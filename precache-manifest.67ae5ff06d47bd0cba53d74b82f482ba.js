cbabe007dca644986206b1f1ece8ebf6b75005f3
Duong Nguyen <dnguyen@bocasay.com>
2019-03-21 17:59:40 +0700

    Updates

---
+++ precache-manifest.67ae5ff06d47bd0cba53d74b82f482ba.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "225d61cf2d61014d4aea",
+    "url": "/ui-components/static/js/runtime~main.ed38a9cb.js"
+  },
+  {
+    "revision": "3ec8ed60f836dcdb278b",
+    "url": "/ui-components/static/js/main.252cf224.chunk.js"
+  },
+  {
+    "revision": "b0a649b66f286899741d",
+    "url": "/ui-components/static/js/2.b1b7e436.chunk.js"
+  },
+  {
+    "revision": "907c13054e65039f1fb732a316a7e491",
+    "url": "/ui-components/index.html"
+  }
+];
